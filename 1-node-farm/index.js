3862e2beb923fc7ed54f20456cf7ce74ef718ecd
thaniel <thanielalves@gmail.com>
2020-01-29 17:07:05 -0300

    15. HTML Templating: Filling the Templates

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -28,17 +28,47 @@ const url = require('url');//manipulador de rotas
 
 ////////////////////////////////////////////
 ///SERVER
+const replaceTemplate = (temp, product)=>{
+    // //g expressao regular para sub. todas as ocorrências
+    let output = temp.replace(/{%PRODUCTNAME%}/g,product.productName);
+    output = output.replace(/{%IMAGE%}/g,product.image);
+    output = output.replace(/{%PRICE%}/g,product.price);
+    output = output.replace(/{%FROM%}/g,product.from);
+    output = output.replace(/{%NUTRIENTS%}/g,product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g,product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g,product.description);
+    output = output.replace(/{%ID%}/g,product.id);
+
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g,'not-organic');
+
+    return output;
+}
+
 //Le o arquivo de forma sincrona
+
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data); //formata o arquivo em obj JSON
 
 const server = http.createServer((req, res) => {    
     const pathName = req.url;
 
+    //Overview page
     if(pathName === '/' || pathName === '/overview'){
-        res.end('This is the OVERVIEW!!');    
+        res.writeHead(200,{'Content-Type': 'text/html'});
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard,el));
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}',cardsHtml);
+        res.end(output);    
+    
+    //Product page
     }else if(req.url === '/products'){
         res.end('This is the PRODUCT!!');
+    
+    //API
     }else if(req.url === '/api'){
 
         console.log(__dirname);//devolve o diretorio local de execução da aplicação 
@@ -46,6 +76,8 @@ const server = http.createServer((req, res) => {
         res.writeHead(200,{'Content-Type': 'application/json'});
         res.end(data);
     }
+
+    //Not found page
     else{
         res.writeHead(404,{ //devolvendo cabeçalhos http personalizado
             'Content-Type': 'text/html',
