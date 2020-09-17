daabbd3a806df587c047f9dfa408d9178667327f
Thaniel Rocha Alves <thaniel.alves@secad.local>
2020-09-16 21:04:24 -0300

    Adicionando Firebase

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,7 +1,25 @@
 const dotenv = require('dotenv');
 const app = require('./app');
+const admin = require("firebase-admin"); //FIREBASE
+const serviceAccount = require("./database/serviceAccountKey.json"); //Dados de autenticacao
 
 dotenv.config({ path: './config.env' });
+
+//Inicializa BD Firebase
+const dburl = process.env.DATABASE_TESTE;
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: `https://${dburl}.firebaseio.com/` //teste
+});
+
+const db = admin.database();//Nó raiz do bd
+
+//consultas
+const query = db.ref('1');
+
+query.on('value',snap1 => {
+  console.log(snap1.val());
+});
 
 //SERVER
 
